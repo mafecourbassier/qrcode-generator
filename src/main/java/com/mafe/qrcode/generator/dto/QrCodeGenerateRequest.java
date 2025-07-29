d9c2651b1e652843a4c79f6df98816a4e24654d2
mafecourbassier <eng.mafee@gmail.com>
2025-07-29 14:29:03 -0300

    feat: implementar DTOs para requisição e resposta

---
+++ src/main/java/com/mafe/qrcode/generator/dto/QrCodeGenerateRequest.java
@@ -0,0 +1,5 @@
+package com.mafe.qrcode.generator.dto;
+
+public record QrCodeGenerateRequest(String text) {
+
+}
---
+++ src/main/java/com/mafe/qrcode/generator/dto/QrCodeGenerateResponse.java
@@ -0,0 +1,4 @@
+package com.mafe.qrcode.generator.dto;
+
+public record QrCodeGenerateResponse(String url) {
+}
